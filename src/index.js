49cd72dc7c93ad37a76ae5a5a890841c2b0b0943
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-03-09 11:59:11 -0700

    Add timeout 'let' test to show babel adding closures

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Wizard from './Wizard';
+
 console.log(new Wizard('Bob'));
 console.log(new Wizard('Ravalynn', 'Frost'));
 
@@ -10,9 +11,19 @@ const component = () => {
   const newObj = { ...obj, c: 'charlie' };
 
   // ES8 Object.values test
+  // Will not transpile without babel polyfills because it is a new method
   console.log(Object.values(newObj));
+  // ES Array.includes test
+  console.log(['a', 'b', 'c'].includes('b'));
 
   return element;
 };
+
+// Asyncronous block scoping test
+for (let i = 0; i < 10; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 1);
+}
 
 document.body.appendChild(component());
