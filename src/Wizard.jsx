26f35fad948f1abb9bd7f1a063096c1ca90d9758
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-27 20:19:18 -0600

    Update example to have pointless state

--- src/Wizard.jsx
+++ src/Wizard.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 // Classes can be assigned just like regular functions (they can also be unnamed)
 class Wizard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spell: '',
+    };
+  }
+
   render() {
     return (
       <div>
         <p>Wizard's name: {this.props.name || 'Vesper'}</p>
         <p>Wizard's school: {this.props.school || 'Fire'}</p>
+        <button onClick={() => this.setState({ spell: 'Fireball' })}>
+          Cast spell
+        </button>
+        {this.state.spell && <p>{this.props.name} casts fireball!</p>}
       </div>
     );
   }
